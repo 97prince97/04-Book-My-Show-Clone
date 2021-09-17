460262dc94c4eee6a1dd9c7c136a8b4b7df3389d
97prince97 <97prince97@gmail.com>
2021-09-17 06:50:46 +0530

    add Premier component

---
+++ src/components/Premier/Premier.component.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import Slider from "react-slick";
+
+// Component
+import Poster from "../Poster/poster.component";
+
+const Premier = () => {
+  const settings = {
+    Infinity: false,
+    autoplay: false,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    InitialSlide: 0,
+    responsive: [
+      {
+        breakpoints: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          Infinity: true,
+        },
+      },
+      {
+        breakpoints: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          InitialSlide: 1,
+        },
+      },
+      {
+        breakpoints: 400,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoints: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          Infinity: true,
+        },
+      },
+    ],
+  };
+
+  const PremierImages = [
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00312484-ehedpyzcmm-portrait.jpg",
+      alt: "The Balcan Lane",
+      title: "The Balcan Lane",
+      subtitle: "English",
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00310648-uwreepnzec-portrait.jpg",
+      alt: "Resistance",
+      title: "Resistance",
+      subtitle: "English",
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302306-xxwtjnexzz-portrait.jpg",
+      alt: "Proximity",
+      title: "Proximity",
+      subtitle: "English",
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00137312-qszmhzktyk-portrait.jpg",
+      alt: "The Secrets We Keep",
+      title: "The Secrets We Keep",
+      subtitle: "English",
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00310648-uwreepnzec-portrait.jpg",
+      alt: "Resistance",
+      title: "Resistance",
+      subtitle: "English",
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00310648-uwreepnzec-portrait.jpg",
+      alt: "Resistance",
+      title: "Resistance",
+      subtitle: "English",
+    },
+  ];
+
+  return (
+    <>
+      <Slider {...settings}>
+        {PremierImages.map((image) => (
+          <Poster {...image} />
+        ))}
+      </Slider>
+    </>
+  );
+};
+
+export default Premier;
